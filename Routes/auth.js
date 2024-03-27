3b808ae1a91f03c71e2bab0f00f2874d42b8047d
Manish201304 <107488431+Manish201304@users.noreply.github.com>
2024-03-27 18:57:54 +0530

    Creating a middleware to decode user from jwt

--- Routes/auth.js
+++ Routes/auth.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const { validationResult, body } = require('express-validator');
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser')
 
-//sending request to localhost:5000/api/auth/createUser
+//Route1 : sending request to localhost:5000/api/auth/createUser
 router.post('/createUser', [
     body('name', 'Please enter at least 3 characters').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
@@ -36,6 +37,7 @@ router.post('/createUser', [
                 id: user.id
             }
         }
+        //Genrating jwt tokens
         const authToken = jwt.sign(data, 'manishisabadboy');
         console.log(authToken);
         res.json({ authToken });
@@ -45,7 +47,7 @@ router.post('/createUser', [
     }
 });
 
-//sending request to localhost:5000/api/auth/login
+//Route2 : sending request to localhost:5000/api/auth/login  for login
 router.post('/login', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password can not be blank').exists(),
@@ -57,21 +59,23 @@ router.post('/login', [
     
     const { email, password } = req.body;
     try {
+        // Checking email exists or not
     let user =  await User.findOne({ email });
     if (!user) {
         return res.status(400).json({ error: "please enter with correct credentials" });
     }
+    // Comapring password with DataBase
     const passwordCompare = await bcrypt.compare(password, user.password);
     if (!passwordCompare) {
         return res.status(400).json({ error: "please enter with correct credentials" });
     }
-    
-    
+
     const data = {
         user: {
             id: user.id
         }
     }
+    //Genrating jwt tokens
     const authToken = jwt.sign(data, 'manishisabadboy');
     console.log(authToken);
     res.json({ authToken });
@@ -81,4 +85,16 @@ router.post('/login', [
 }
 });
 
+//Route3 : sending request to localhost:5000/api/auth/userdetails  for user details
+router.post('/userdetails', fetchuser, async (req, res) => {
+    try {
+        userId = req.user.id;
+        const user = await User.findById(userId).select("-password");
+        res.send(user);
+    } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+}
+    
+})
 module.exports = router;
