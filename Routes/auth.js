c4726feacf790eabb3e47aabf69bcd5b736eb914
Manish201304 <107488431+Manish201304@users.noreply.github.com>
2024-03-27 16:38:18 +0530

    insert validators

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,26 +1,37 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
-const {validationResult, body } = require('express-validator');
+const { validationResult, body } = require('express-validator');
 
-router.post('/',[
-    body('name', 'Plz enter atleast 3 characters ').isLength({min: 3}),
+//sending request to localhost:5000/api/auth/createUser
+router.post('/createUser', [
+    body('name', 'Please enter at least 3 characters').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
-    body('password', 'plz enter password haning atleast 5 characters').isLength({min: 5})
-
-], (req, res)=>{
+    body('password', 'Please enter a password containing at least 5 characters').isLength({ min: 5 })
+], async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
-    
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(User=>res.json(User)).catch(err=>{console.log(err)
-        res.json({error: 'plz enter a unique value'})});
-    
-})
 
-module.exports = router
+    try {
+        // checking whether this email already exists
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry, a user with this email already exists" });
+        }
+        //Creating a new user
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        res.json({ user });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+module.exports = router;
